9bb774f7748cfb9eeaf72ba099eb86731239e212
Dmitriy Pechurkin <d.pechurkin@goodt.me>
2022-06-28 16:32:20 +0500

    change method

--- DCore.php
+++ DCore.php
@@ -1,4 +1,4 @@
-<? 
+<?
 
 
 /**
@@ -12,57 +12,91 @@
 class DCore
 {	
 	static $path_files= '/files_log/';
-	//функция сохранения файлов 
-	// data - данные которые нужно сохранить , файл массив строка, что угодно
-	// file - названиеп файла под которым будет сохраненны данные
-	// serial - нужно ли серрилизовать переменную для записи в файл , полезно если сохраняешь массивы после чтения файла производишь обратную сериализацию и у тебя готовая переменная
-	// file_append - нужно ли добавлять данные к файлу, можно делать что то типа логов 
-	static function SaveData($data, $file='' ,$serial=false,$file_append=false){
-		$file=$file==''?'log_default.txt':$file;
-		$flag=false;
-		if($data) $flag=true;
-		if($serial) $data = serialize($data);
 
-		$pach=$_SERVER['DOCUMENT_ROOT'].DCore::$path_files.$file;
-		if($flag){
-			if($file_append)
-				file_put_contents($pach, $data, FILE_APPEND);
-			else 
-				file_put_contents($pach, $data);
-			
-			return 'data writed done'; 
-		}
-		return 'not data to write'; 
-	}
+    /**
+     * функция сохранения данных в вайл , может перезаписывать или добавлять данные в файл
+     * @param $data - object данные которые нужно записать в виде строки или обьекта
+     * @param string $file - string путь и название файла относительно папки экспортов
+     * @param bool $serial - boolean переводить обьект в строку посредство кодирования в json
+     * @param bool $file_append - boolean добавлять данные в файл
+     * @return string
+     */
+    static function saveData($data, $file = '', $serial = false, $file_append = false) {
+        $file = $file == '' ? 'log_default.txt' : $file;
+        if ($serial) $data = json_encode($data, JSON_UNESCAPED_UNICODE);
+        $path = DCore::$path_files . $file;
+        DCore::checkPath($path);
+        if ($file_append)
+            file_put_contents($path, $data . PHP_EOL, FILE_APPEND);
+        else
+            file_put_contents($path, $data);
 
-	//функция извлечение информации из файлов 
-	static function GetData($file='',$serial=false){
-		$file=$file==''?'log_default.txt':$file;
-		$pach=$_SERVER['DOCUMENT_ROOT'].DCore::$path_files.$file;
-		$data=file_get_contents($pach);
-		if($serial) $data = unserialize($data);
-		return $data;
-	}
+        return 'data writed done';
+    }
 
-	// функция проверки главной страницы, если в параметр передана строка, то функция вернёт её если это главная страница
-	public function TM($str = null){
-		global $APPLICATION;
-		if ($str =='') 
-		   $str = true ; 
-		if ($APPLICATION->GetCurPage(true) == SITE_DIR.'index.php')
-		   return $str;
-		else 
-		   return false;
-	}
+    /**
+     * функция получения данных из файла
+     * @param string $file - string путь до файла
+     * @param bool $serial - boolean декодировать строку из json
+     * @return false|mixed|string
+     */
+    static function getData($file = '', $serial = false) {
+        $file = $file == '' ? 'log_default.txt' : $file;
+        $path = DCore::$path_files . $file;
+        $data = file_get_contents($path);
+        if ($serial) $data = json_decode($data);
+        return $data;
+    }
 
-	// функция проверки что страница не главная, если в параметр передана строка, то функция вернёт её если это не главная страница
-	public function TNM($str = null){
-		global $APPLICATION;
-		if ($str =='') 
-		   $str = true ; 
-		if ($APPLICATION->GetCurPage(true) != SITE_DIR.'index.php')
-		   return $str;
-		else 
-		   return false;
-	}
+    /**
+     * функция проверяет и создаёт папки если их нет для сохранения данных в файл
+     * @param $path - string относительный путь
+     * @return bool
+     */
+    static function checkPath($path) {
+        $arPath = explode('/', $path);
+        $testPath = '';
+        $end = count($arPath) - 1;
+        foreach ($arPath as $level => $dir) {
+            if ($testPath == '') {
+                $testPath = $dir;
+            } else {
+                if ($end > $level)
+                    $testPath .= '/' . $dir;
+            }
+        }
+        if (!is_dir($testPath)) {
+            mkdir($testPath, 0775, true);
+        }
+        return true;
+    }
+
+    /**
+     * функция для вывода данных в командную строку для отображение хода выполнения скрипта
+     * @param String $type - строка. служит для определения типа выводимового сообщения
+     * @param String $message - служит для вывода сообщения
+     * @param bool $rewrite - условный параметр для вывода символа перевода строки в конце сообщения или вывода символа очищения текущей строки от сообщения,
+     *                        используется для вывода сообщений об ожидании ответа от запроса к апи
+     */
+    public function eLog($type,$message,$rewrite = false){
+        echo date("Y.m.d H:i:s").' '.$type .' '.$message.($rewrite? "\r":PHP_EOL);
+    }
+
+    /**
+     * функция генерации уникального строки
+     * @param int $lenght - длинна уникальной строки
+     * @return bool|string - возвращает уникальную строку которую можно использовать как уникальный индетификатор
+     * @throws Exception - выдает ошибку в случае если нет необходимой библиотеки
+     */
+    public function uniqidReal($lenght = 13) {
+        // uniqid gives 13 chars, but you could adjust it to your needs.
+        if (function_exists("random_bytes")) {
+            $bytes = random_bytes(ceil($lenght / 2));
+        } elseif (function_exists("openssl_random_pseudo_bytes")) {
+            $bytes = openssl_random_pseudo_bytes(ceil($lenght / 2));
+        } else {
+            throw new Exception("no cryptographically secure random function available");
+        }
+        return substr(bin2hex($bytes), 0, $lenght);
+    }
 }
